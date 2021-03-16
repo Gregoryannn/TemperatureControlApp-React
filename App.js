456034b4f59b0758ee3c0fbfcf116f446d0a4679
gregoryann <32854050+gregoryann@users.noreply.github.com>
2021-03-16 19:34:54 +0100

    update App.js

--- App.js
+++ App.js
@@ -1,34 +1,47 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./styles.css";
 
 const App = () => {
     const [temperatureValue, setTemperatureValue] = useState(10);
-    const [temperatureValue, setTemperatureValue = useState("cold")]
+    const [temperatureColor, setTemperatureColor] = useState("cold");
 
-    returtn(
+    const increaseTemperature = () => {
+        if (temperatureValue === 30) return;
 
-        <
+        const newTemperature = temperatureValue + 1;
+
+        setTemperatureValue(newTemperature);
+        if (newTemperature >= 15) {
+            setTemperatureColor("hot");
+        }
+    };
+
+    const decreaseTemperature = () => {
+        if (temperatureValue === 0) return;
+
+        const newTemperature = temperatureValue - 1;
+        setTemperatureValue(newTemperature);
+        if (newTemperature < 20) {
+            setTemperatureColor("cold");
+        }
+    };
+
+    return ( <
         div className = "app-container" >
         <
         div className = "temperature-display-container" >
         <
-        div className = "temperature-display" > 10 C < /div>   <
-        /div>
-
+        div className = { `temperature-display ${temperatureColor}` } > { temperatureValue }°
+        C <
+        /div> <
+        /div> <
+        div className = "button-container" >
         <
-        div className = "button-container" > < /div>  <
-        button onClick = {
-            () => setTemperatureValue(temperatureValue + 1)
-        } > + < /button> <
-        button onClick = {
-            () => setTemperatureValue(temperatureValue + 1) } > - < /button>
-
-        <
-        /div>  <
+        button onMouseDown = { increaseTemperature } > + < /button> <
+        button onClick = { decreaseTemperature } > - < /button> <
+        /div> <
         /div>
-
-    )
-
-}
-
+    );
+};
 
 export default App;
