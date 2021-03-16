78d9b3a7c85ea5135a8626d6f9565d2e63da5c40
gregoryann <32854050+gregoryann@users.noreply.github.com>
2021-03-16 19:31:54 +0100

    create styles.css

--- App.js
+++ App.js
@@ -2,20 +2,23 @@ import React from 'react';
 
 const App = () => {
     const [temperatureValue, setTemperatureValue] = useState(10);
+    const [temperatureValue, setTemperatureValue = useState("cold")]
 
-    returtn( <
+    returtn(
+
         <
         div className = "app-container" >
         <
         div className = "temperature-display-container" >
         <
-        div className = "temperature-display" > 10 C < /div>  <
-        /div >
+        div className = "temperature-display" > 10 C < /div>   <
+        /div>
 
         <
-        div className = "button-container" > < /div> <
+        div className = "button-container" > < /div>  <
         button onClick = {
-            () => setTemperatureValue(temperatureValue + 1) } > + < /button>  <
+            () => setTemperatureValue(temperatureValue + 1)
+        } > + < /button> <
         button onClick = {
             () => setTemperatureValue(temperatureValue + 1) } > - < /button>
 
