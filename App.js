821999f53d6c6c594dd3c4f5dc5f944ca4a4e12a
gregoryann <32854050+gregoryann@users.noreply.github.com>
2021-03-16 18:53:00 +0100

    update App.js

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
 const App = () => {
+    const [temperatureValue, setTemperatureValue] = useState(20);
+
     returtn( <
-        div > hello React! < /div>
+        <
+        div className = "app-container" >
+        <
+        div className = "temperature-display-container" >
+        <
+        div className = "temperature-display" > 10 C < /div> < /
+        div >
 
+        <
+        div className = "button-container" > < /div> <
+        button > + < /button> <
+        button > - < /button>
 
-
+        <
+        /div> < /
+        div >
 
     )
 
+}
 
-};
+
+export default App;
