6b1a428a88db396cc038c9b31c3a589239975f52
gregoryann <32854050+gregoryann@users.noreply.github.com>
2021-03-16 19:34:21 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
 
+import App from "./App";
+
+const rootElement = document.getElementById("root");
 ReactDOM.render( <
     React.StrictMode >
     <
     App / >
-
     <
-    /React.StrictMode>
-    document.getElementById('root')
-
+    /React.StrictMode>,
+    rootElement
 );
